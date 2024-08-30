e38bcb61d88aeacf53ecb8cb40a379130648a95c
Liun Tappolet <104633295+Liuuner@users.noreply.github.com>
2024-08-30 20:06:16 +0200

    Update Utils.ts

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,5 +1,5 @@
 function easeOutCubic(t: number): number {
-    return 1 - Math.pow(1 - t, 3);
+    return 1 - Math.pow(1 - t, 2);
 }
 
 const getFromLsOrElse = <T>(key: string, defaultValue: T): T => {
@@ -17,8 +17,18 @@ const toReversed = (a: any[]) => {
     return [...a].reverse()
 }
 
+function cubicBezier(t: number, p0: number, p1: number, p2: number, p3: number): number {
+    const t2 = t * t;
+    const t3 = t2 * t;
+    const mt = 1 - t;
+    const mt2 = mt * mt;
+    const mt3 = mt2 * mt;
+    return mt3 * p0 + 3 * mt2 * t * p1 + 3 * mt * t2 * p2 + t3 * p3;
+}
+
 export {
     easeOutCubic,
     getFromLsOrElse,
-    toReversed
+    toReversed,
+    cubicBezier
 };
